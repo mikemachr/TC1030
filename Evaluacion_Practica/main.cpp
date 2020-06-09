41f40b35a844215195763e739e986cb563d7e114
a01620402@itesm.mx <Mik3machr33>
2020-06-09 09:57:24 -0500

    mamadas del uml

--- Evaluacion_Practica/main.cpp
+++ Evaluacion_Practica/main.cpp
@@ -15,7 +15,6 @@ private:
 string matricula, plan;
 public:
     Estudiante(string, string, string);
-    string getName();
     string getMatricula();
     string getPlan();
     void getAttrs(); 
@@ -28,9 +27,6 @@ plan=_plan;
 matricula=_matricula;
 }
 
-string Estudiante::getName(){
-    return nombre;
-}
 
 string Estudiante::getMatricula(){
     return matricula;
@@ -44,7 +40,7 @@ string Estudiante::getPlan(){
 
 
 void Estudiante::getAttrs(){
-    cout<<"Nombre: "<<getName()<<endl;
+    cout<<"Nombre: "<<nombre<<endl;
     cout<<"Matricula: "<<getMatricula()<<endl;
     cout<<"Plan de estudios: "<<getPlan()<<endl<<endl<<endl;
 }
@@ -55,20 +51,14 @@ private:
 string nomina, departamento;
 public:
     Profesor(string,string,string);
-    string getName();
     string getNomina();
     string getDepartamento();
     void getAttrs(); 
 };
-
 Profesor::Profesor(string _nombre, string _nomina, string _departamento){
 nombre=_nombre;
 departamento=_departamento;
 nomina=_nomina;
-}
-
-string Profesor::getName(){
-    return nombre;
 }
 
 string Profesor::getNomina(){
@@ -80,16 +70,16 @@ string Profesor::getDepartamento(){
 }
 
 void Profesor::getAttrs(){
-    cout<<"Nombre: "<<getName()<<endl;
+    cout<<"Nombre: "<<nombre<<endl;
     cout<<"Nomina: "<<getNomina()<<endl;
     cout<<"Departamento: "<<getDepartamento()<<endl<<endl<<endl;
 }
 
 int main(){
-    Estudiante E1("Juan Sánchez","A01234567","Ing. Mecatronica");
-    Estudiante E2("Monica Pérez","A01223344","Ing. en Tecnologias Computacionales");
+    Estudiante E1("Juan Sanchez","A01234567","Ing. Mecatronica");
+    Estudiante E2("Monica Perez","A01223344","Ing. en Tecnologias Computacionales");
     Estudiante E3("Carlos Augusto Amador","A01234455","Ing. Industrial");
-    Profesor P1("Ernesto Ramírez","L00700700","Departamento de Computacion");
+    Profesor P1("Ernesto Ramirez","L00700700","Departamento de Computacion");
     Profesor P2("Jose Antonio Rodriguez","L099999","Departamento de Ciencias Basicas");
     Persona* Personas[5];
     Personas[0] = &E1;
